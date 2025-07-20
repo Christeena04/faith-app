690af9edefa4ed0c86d6956c990387af393eb69a
Christeena04 <149092954+Christeena04@users.noreply.github.com>
2025-07-20 16:39:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const verses = [
   "God is within her, she will not fall. – Psalm 46:5",
   "Trust in the Lord with all your heart. – Proverbs 3:5",
-  "Be still and know that I am God. – Psalm 46:10"
+  "Be still and know that I am God. – Psalm 46:10",
+  "For I know the plans I have for you, plans to prosper you and not to harm you, plans to give you hope and a future. - Jeremiah 29:11",
+  "I can do all this through him who gives me strength. - Philippians 4:13",
+  "The Lord is my shepherd, I lack nothing. - Psalm 23:1",
+  "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. - John 3:16",
+  "Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go. - Joshua 1:9",
+  "The joy of the Lord is your strength. - Nehemiah 8:10",
+  "Cast all your anxiety on him because he cares for you. - 1 Peter 5:7",
+   "Trust in the Lord with all your heart and lean not on your own understanding. - Proverbs 3:5",
+    "Come to me, all you who are weary and burdened, and I will give you rest. - Matthew 11:28",
+   "God is within her, she will not fall. - Psalm 46:5",
+ "For I know the plans I have for you, plans to prosper you and not to harm you, plans to give you hope and a future. - Jeremiah 29:11",
+  "I can do all this through him who gives me strength. - Philippians 4:13",
+  "The Lord is my shepherd, I lack nothing. - Psalm 23:1",
+  "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. - John 3:16",
+  "Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go. - Joshua 1:9",
+ "The joy of the Lord is your strength. - Nehemiah 8:10"
+  
+  
+  
 ];
 
 // When "Today's Verse" button is clicked → open the verse box
